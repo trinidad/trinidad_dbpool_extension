7e4c9d16af9f8b22e28815dee4c9218b7c2a38a6
David Calavera <david.calavera@gmail.com>
2010-03-24 10:40:46 +0100

    cleaning the extension api

--- lib/trinidad-dbpool/webapp_addon.rb
+++ lib/trinidad-dbpool/webapp_addon.rb
@@ -1,15 +1,26 @@
 module Trinidad
   class DbpoolWebAppAddon
-    def self.configure_internal(app_context, options)
+    def configure(*args)
+      puts 'Configuring database connection pool'
+      splat = *args
+      create_resource(splat[0], splat[1], splat[2])
+    end
+
+    def create_resource(app_context, class_loader, options)
       resource = Trinidad::Tomcat::ContextResource.new
       resource.setAuth("Container")
       resource.setName(options.delete('name'))
       resource.setType("javax.sql.DataSource")
-      resource.setDescription(options.delete('description')) if options['description']
+      resource.setDescription(options.delete('description')) if options.has_key?('description')
+
       options.each do |key, value|
         resource.setProperty(key.to_s, value.to_s)
       end
 
+      class_loader.addURL(java.io.File.new(driver_jar).to_url)
+      resource.setProperty("driverClassName", driver_name)
+
+      app_context.naming_resources.add_resource(resource)
       resource
     end
   end
--- lib/trinidad-mysql-dbpool-extension/mysql_webapp_addon.rb
+++ lib/trinidad-mysql-dbpool-extension/mysql_webapp_addon.rb
@@ -1,20 +1,11 @@
 module Trinidad
   class MysqlDbpoolWebAppAddon < DbpoolWebAppAddon
-    def self.configure(*args)
-      puts 'Configuring Mysql connection pool'
-      args = args.flatten
-      configure_internal(args[0], args[1], args[2])
+    def driver_jar
+      File.dirname(__FILE__) + '/../../trinidad-libs/mysql-connector-java-5.1.12-bin.jar'
     end
 
-    def self.configure_internal(app_context, class_loader, options)
-      resource = DbpoolWebAppAddon.configure_internal(app_context, options)
-
-      mysql_driver_jar = File.dirname(__FILE__) + '/../../trinidad-libs/mysql-connector-java-5.1.12-bin.jar'
-      class_loader.addURL(java.io.File.new(mysql_driver_jar).to_url)
-
-      resource.setProperty("driverClassName", "com.mysql.jdbc.Driver")
-
-      app_context.naming_resources.add_resource(resource)
+    def driver_name
+      "com.mysql.jdbc.Driver"
     end
   end
 end
