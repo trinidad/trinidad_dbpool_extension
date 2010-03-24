137b44e3bff94540452bf5c1c2704c4cb6d05b29
David Calavera <david.calavera@gmail.com>
2010-03-24 01:37:23 +0100

    setting resource type

--- lib/trinidad-dbpool/webapp_addon.rb
+++ lib/trinidad-dbpool/webapp_addon.rb
@@ -1,11 +1,10 @@
 module Trinidad
   class DbpoolWebAppAddon
     def self.configure_internal(app_context, options)
-      options[:type] = "javax.sql.DataSource"
-
       resource = Trinidad::Tomcat::ContextResource.new
       resource.setAuth("Container")
       resource.setName(options.delete('name'))
+      resource.setType("javax.sql.DataSource")
       resource.setDescription(options.delete('description')) if options['description']
       options.each do |key, value|
         resource.setProperty(key.to_s, value.to_s)
