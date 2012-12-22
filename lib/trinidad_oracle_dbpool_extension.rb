7cd0a40a342c81458f43d35ed56421ad574abd2d
kares <self@kares.org>
2012-12-22 14:07:48 +0100

    load the oracle jar (instead of a require) just like others

--- lib/trinidad_oracle_dbpool_extension.rb
+++ lib/trinidad_oracle_dbpool_extension.rb
@@ -1,3 +1,3 @@
 require 'trinidad_dbpool'
-require File.expand_path('../../trinidad-libs/ojdbc14', __FILE__)
+load File.expand_path('../../trinidad-libs/ojdbc14.jar', __FILE__)
 require 'trinidad_oracle_dbpool_extension/oracle_webapp_extension'
